7a2fe87b6791485f8bf2b19ead0221412b5950f2
Ashley Boehs <ashley@boehs.com>
2023-10-10 09:34:53 -0500

    Add ShortCode base62 encoding

---
+++ app/models/short_code.rb
@@ -0,0 +1,26 @@
+class ShortCode
+  ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".freeze
+  BASE = ALPHABET.length
+
+
+  def self.encode(number)
+    return ALPHABET.first if number.zero? || number.nil?
+
+    result = ""
+
+    while number > 0 do
+      index = number % BASE
+      char = ALPHABET[index]
+      result.prepend char
+      number = number / BASE 
+    end
+
+    result
+  end  
+
+  def self.decode(string)
+  end 
+end 
+
+# Base62.encode(1824)
+# Base62.decode("gw")
